cce440ca322961bd2728b72ebe280cde655eb024
syntialai <53588149+syntialai@users.noreply.github.com>
2021-05-15 01:49:07 +0700

    Setup layout for HomeFragment and hospital RecyclerView item

---
+++ android-app/app/src/main/java/com/bangkit/team18/qhope/ui/home/view/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.bangkit.team18.qhope.ui.home.view
+
+import android.view.View
+import com.bangkit.team18.qhope.databinding.FragmentHomeBinding
+import com.bangkit.team18.qhope.ui.base.view.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::inflate) {
+
+  override fun setupViews() {
+    TODO("Not yet implemented")
+  }
+
+  override fun onClick(v: View?) {
+    TODO("Not yet implemented")
+  }
+}
