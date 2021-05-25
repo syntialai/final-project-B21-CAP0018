173c2ef1ed2b199af9a16333a63b84116c718ed3
syntialai <53588149+syntialai@users.noreply.github.com>
2021-05-25 19:10:59 +0700

    Downgrade to remove unexpected bugs

--- android-app/app/src/main/java/com/bangkit/team18/qhope/di/FeatureModules.kt
+++ android-app/app/src/main/java/com/bangkit/team18/qhope/di/FeatureModules.kt
@@ -1,7 +1,17 @@
 package com.bangkit.team18.qhope.di
 
+import com.bangkit.team18.qhope.ui.booking.viewmodel.BookingConfirmationViewModel
+import com.bangkit.team18.qhope.ui.booking.viewmodel.HospitalDetailViewModel
+import com.bangkit.team18.qhope.ui.history.viewmodel.HistoryDetailViewModel
+import com.bangkit.team18.qhope.ui.history.viewmodel.HistoryViewModel
+import com.bangkit.team18.qhope.ui.home.viewmodel.HomeViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
-
+  viewModel { HomeViewModel(get()) }
+  viewModel { HospitalDetailViewModel(get()) }
+  viewModel { BookingConfirmationViewModel(get()) }
+  viewModel { HistoryViewModel() }
+  viewModel { HistoryDetailViewModel() }
 }
