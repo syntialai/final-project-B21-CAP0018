2ceee5a5a8920141f73cb514ec6c264a4cb39972
Steven <stevenzlo122@gmail.com>
2021-06-01 11:29:38 +0700

    fixing bug in registration

--- android-app/app/src/main/java/com/bangkit/team18/qhope/ui/base/view/BaseActivityViewModel.kt
+++ android-app/app/src/main/java/com/bangkit/team18/qhope/ui/base/view/BaseActivityViewModel.kt
@@ -5,6 +5,7 @@ import android.app.Dialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
