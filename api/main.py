abca269095e9dcfbec479d471bc2fe83760e35db
Syntia <53588149+syntialai@users.noreply.github.com>
2021-10-25 21:45:06 +0700

    Add Sample API

--- api/main.py
+++ api/main.py
@@ -1,19 +1,43 @@
-import requests
-
-def verify_identity(event, context):
-    """Triggered by a change to a Firestore document.
-    Args:
-         event (dict): Event payload.
-         context (google.cloud.functions.Context): Metadata for the event.
+import firebase_admin
+import os
+from flask import Flask, request, jsonify
+from firebase_admin import credentials, firestore, initialize_app
+app = Flask(__name__)
+cred = credentials.Certificate("./test-key.json")
+default_app = initialize_app(cred)
+db = firestore.client()  # this connects to our Firestore database
+collection = db.collection('hospital_data/1000030/room_data')  # opens 'places' collection
+doc = collection.document('1000030_business')  # specifies the 'rome' document
+@app.route('/')
+def test():
+    return "hello world"
+    
+@app.route('/hospital_data/1000030/room_data', methods=['POST'])
+def create():
     """
-    resource_string = context.resource
-    userId = resource_string.split("/")[-1] # This is the userId
-    oldVerificationStatus = event["oldValue"]["fields"]["verification_status"]["stringValue"]
-    verificationStatus = event["value"]["fields"]["verification_status"]["stringValue"] # This is the verification_status
-    if (oldVerificationStatus != verificationStatus and verificationStatus == "UPLOADED"):
-        ktpUrl = event["value"]["fields"]["ktp_url"]["stringValue"]
-        selfieUrl = event["value"]["fields"]["selfie_url"]["stringValue"]
-        # Call the API here 
-        payload = { 'userId': userId, 'ktp_url': ktpUrl, 'selfie_url': selfieUrl }
-        r = requests.request("POST", url="http://34.101.79.70:4321/register", json=payload)
-        print(f"Changed by user: {userId} with status {verificationStatus}, data ktp : {ktpUrl}, data selfie: {selfieUrl}")
+        create() : Add document to Firestore collection with request body
+        Ensure you pass a custom ID as part of json body in post request
+        e.g. json={'id': '1', 'title': 'Write a blog post'}
+    """
+    try:
+        available_room = request.json['available_room']
+        collection.document("1000030_business").set(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+@app.route('/hospital_data/1000030/room_data', methods=['PUT'])
+def update():
+    """
+        update() : Update document in Firestore collection with request body
+        Ensure you pass a custom ID as part of json body in post request
+        e.g. json={'id': '1', 'title': 'Write a blog post today'}
+    """
+    try:
+        available_room = request.json['available_room']
+        collection.document("1000030_economy").update(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+# port = int(os.environ.get('PORT', 80))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6543)
