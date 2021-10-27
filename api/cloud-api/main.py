59a134dd7395acf2aefdd9d89c5dcc12960a280e
syntialai <53588149+syntialai@users.noreply.github.com>
2021-10-27 21:38:45 +0700

    Init api

---
+++ api/cloud-api/main.py
@@ -0,0 +1,117 @@
+import werkzeug
+from firebase_admin import credentials, firestore, initialize_app
+from flask import Flask, request, jsonify
+from werkzeug.exceptions import HTTPException
+
+app = Flask(__name__)
+cred = credentials.Certificate('D:/BANGKIT/Capstone Project/final-project-B21-CAP0018/api/cloud-api/test-key.json')
+default_app = initialize_app(cred)
+db = firestore.client()  # this connects to our Firestore database
+collection = db.collection('hospital_data/1000030/room_data')  # opens 'places' collection
+doc = collection.document('1000030_business')  # specifies the 'rome' document
+
+COLLECTION_TRANSACTIONS = 'transaction'
+
+
+def getSuccessResponse(response):
+    return jsonify(response), 200
+
+
+def mapToDictionary(documents):
+    mappedDocuments = []
+    for document in documents:
+        mappedDocuments.append(document.to_dict())
+    return mappedDocuments
+
+
+# ===================== Error handling ============================
+@app.errorhandler(werkzeug.exceptions.BadRequest)
+def handle_bad_request(error):
+    return jsonify(
+        code=error.code,
+        status="BAD_REQUEST",
+        message=error.description
+    )
+
+
+@app.errorhandler(werkzeug.exceptions.Unauthorized)
+def handle_unauthorized(error):
+    return jsonify(
+        code=error.code,
+        status="UNAUTHORIZED",
+        message=error.description
+    )
+
+
+@app.errorhandler(werkzeug.exceptions.NotFound)
+def handle_not_found(error):
+    return jsonify(
+        code=error.code,
+        status="NOT_FOUND",
+        message=error.description
+    )
+
+
+# ===================== End of Error handling ============================
+
+@app.route('/transactions', methods=['GET'])
+def getAllTransactions():
+    userId = request.args.get('user_id')
+    hospitalId = request.args.get('hospital_id')
+
+    if userId is not None:
+        transactionsDoc = db.collection(COLLECTION_TRANSACTIONS).where('user_id', '==', userId).stream()
+        result = mapToDictionary(transactionsDoc)
+        return getSuccessResponse(result)
+
+    if hospitalId is not None:
+        transactionsDoc = db.collection(COLLECTION_TRANSACTIONS).where('hospital_id', '==', hospitalId).stream()
+        result = mapToDictionary(transactionsDoc)
+        return getSuccessResponse(result)
+
+    raise werkzeug.exceptions.BadRequest()
+
+
+@app.route('/transactions/<id>', methods=['GET'])
+def getTransactionsById(id):
+    transactionDoc = db.collection(COLLECTION_TRANSACTIONS).document(id).get()
+
+    if not transactionDoc.exists:
+        raise werkzeug.exceptions.NotFound()
+
+    return getSuccessResponse(transactionDoc.to_dict())
+
+
+@app.route('/hospital_data/1000030/room_data', methods=['POST'])
+def create():
+    """
+        create() : Add document to Firestore collection with request body
+        Ensure you pass a custom ID as part of json body in post request
+        e.g. json={'id': '1', 'title': 'Write a blog post'}
+    """
+    try:
+        available_room = request.json['available_room']
+        collection.document("1000030_business").set(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+@app.route('/hospital_data/1000030/room_data', methods=['PUT'])
+def update():
+    """
+        update() : Update document in Firestore collection with request body
+        Ensure you pass a custom ID as part of json body in post request
+        e.g. json={'id': '1', 'title': 'Write a blog post today'}
+    """
+    try:
+        available_room = request.json['available_room']
+        collection.document("1000030_economy").update(request.json)
+        return jsonify({"success": True}), 200
+    except Exception as e:
+        return f"An Error Occured: {e}"
+
+
+# port = int(os.environ.get('PORT', 80))
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6543)
