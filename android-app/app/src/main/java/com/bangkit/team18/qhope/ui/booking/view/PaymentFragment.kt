8ae39feb388477ecd54e04deb31e22087ea616b7
syntialai <53588149+syntialai@users.noreply.github.com>
2021-11-26 00:56:23 +0700

    Add fragment payment

---
+++ android-app/app/src/main/java/com/bangkit/team18/qhope/ui/booking/view/PaymentFragment.kt
@@ -0,0 +1,24 @@
+package com.bangkit.team18.qhope.ui.booking.view
+
+import android.view.View
+import com.bangkit.team18.qhope.databinding.FragmentPaymentBinding
+import com.bangkit.team18.qhope.ui.base.view.BaseFragment
+import com.bangkit.team18.qhope.ui.base.viewmodel.BaseViewModel
+
+class PaymentFragment : BaseFragment<FragmentPaymentBinding, BaseViewModel>(
+  FragmentPaymentBinding::inflate,
+  BaseViewModel::class
+) {
+
+  override fun setupViews() {
+    TODO("Not yet implemented")
+  }
+
+  override fun onClick(v: View?) {
+    TODO("Not yet implemented")
+  }
+
+  override fun setupObserver() {
+    super.setupObserver()
+  }
+}
